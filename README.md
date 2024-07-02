4e153c12491dea84fd8900c0bde87aafb364d973
David Nester <34682818+dnester@users.noreply.github.com>
2024-07-02 11:36:01 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-cop-migration-profiler
+# Coverity on Polaris Environment Evaluation Utility
+
+## Description
+
+A utility to evaluate the current environment of Coverity on Polaris. The project is a collection of scripts that will create an output which provides:
+
+- Application List
+- Projects / Repositories
+- Users
+- Groups
+
+The script is offered in two types: via main (all scripts execute) and the individual scripts (located in the scripts folder).
+
+### Main Script
+This will execute all scripts which provides a CSV output which will provide all information described above.
+
+### Scripts Folder
+
+- `getApplicationList`
+- `getProjectBranches`
+- `getProjectProperties`
+- `getProjectUserInformation`
+- `setProjectProperties`: Optional, this script will set key/value pairs for each project. Once the scripts have been executed, the output will create a JSON file which you may then set key/value pairs. The `setProjectProperties` will post the key/value pairs to the projects.
+
+## Usage
+
+Review the `CONFIG.JSON` file and populate with the appropriate account information. At a minimum, you must set:
+
+1. Customer Tenant (`customer`)
+2. Email Address
+3. API Token
+4. Password
+
+## Output
+
+There are multiple output files which are created in both JSON and CSV formats. If running the `main.js` script, `finalProjectDetails.csv` will be created in the output folder.
+
+## Changelog
+
+### May 21, 2024
+- Initial build
+
+### May 28, 2024
+- Addition of pulling group information for all users. There are three files created (`grouplist.json`, `userlist.json`, `userlist.csv`)
+
+### July 1, 2024
+- Addition of a main script (entry point) for all-in-one script to complete a full assessment.
+- Addition of a reporting script to combine all results from all scripts to a single, combined report (CSV Output).
--- src/main.mjs
+++ src/main.mjs
@@ -1,25 +1,64 @@
 /* ==========================================================================================================
  * 
- *
  *      Description:
- *  
- *          Phase two of the script will read from the created file "projectList.json".  Prior to 
- *          running this script, please review the projectList file and update with the information
- *          you would like pubished to the coverity on polaris.  You may remove any projects you 
- *          do not need to update and update any key/value pairs you would like to post to the project.
- *          To run the second phase script: 
  * 
- *                  node ./main.mjs
+ *          A utility to evaluate the current environment of Coverity on Polaris.  The project is a collection
+ *          of scripts that will create an output which provides:
+ * 
+ *            - Application List
+ *            - Projects / Repositories
+ *            - Users
+ *            - Groups
+ *  
+ *          The script is offered in two types:  via main (all scripts execute) and the individual scripts 
+ *          (located in the scripts folder). 
+ * 
+ *             a) Main Script:  This will execute all scripts which provides a CSV output which will provide 
+ *                all information described above.
+ * 
+ *             b) Scripts folder   
+ * 
+ *                  - getApplicationList
+ *                  - getProjectBranches
+ *                  - getProjectProperties:  
+ *                  - getProjectUserInformation
+ *                  - setProjectProperties: Optional, this script will set key/value pairs for each project.
+ *                    Once the scripts have been executed, the output will create a json file which you may 
+ *                    then set key/value pairs.  The setProjectProperties will post the key/value pairs to the
+ *                    projects.
  * 
  * 
  *      Usage:
  *          
- *          Review the CONFIG.JSON file and populate the email and password information for the user 
- *          running the script.
+ *          Review the CONFIG.JSON file and populate with the appropriate account information.  At a minimum, you
+ *          must set:
+ * 
+ *            1.  Customer Tenant (customer)
+ *            2.  Email Address
+ *            3.  API Token
+ *            4.  Password
+ * 
+ *
+ * 
+ *      Output:
+ * 
+ *          There are multiple output files which are created in both JSON and CSV formats.  If running the main.js
+ *          script, finalProjectDetails.csv will be created in the output folder.
+ * 
  * 
  *      Date: 
  *          
  *          May 21, 2024 -- Initial build
+ *          
+ *          May 28, 2024
+ *                  -- Addition of pulling group information for all users.   There are three files created
+ *                     (grouplist.json, userlist.json, userlist.csv)
+ * 
+ *          July 1, 2024
+ *                  --  Addition of a main script (entry point) for all-in-one script to complete a full 
+ *                      assessment.
+ *                  --  Addition of a reporting script to combine all results from all scripts to a single, 
+ *                      combined report (CSV Output).
  * 
  * 
  * 
